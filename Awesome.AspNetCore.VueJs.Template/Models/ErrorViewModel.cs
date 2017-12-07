5acffdb3b503a7f9c451596be83905dcef883ab6
xfh11794 <xufenghui@126.com>
2017-12-07 14:58:53 +0800

    初始化项目

---
+++ Awesome.AspNetCore.VueJs.Template/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TCBase.AspNetCore.VueJs.Template.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Awesome.AspNetCore.VueJs.Template/readme.txt
@@ -0,0 +1,3 @@
+1.输入命令 npm i 或者 cnpm i 或者 yarn 安装npm包
+2.webpack 编译VueJs
+2.F5 运行项目
